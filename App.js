55293cffee0c21e711cafaca8871146d747f442d
AX <ax@MacBook-Pro-NS.local>
2024-01-19 20:30:14 +0300

    pretty code

--- App.js
+++ App.js
@@ -3,43 +3,42 @@ import { AddTask } from "./components/AddTask";
 import { TaskWrapper } from "./components/TaskWrapper";
 import tasksReducer from "./components/tasksReducer";
 
+export default function App() {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
 
-
-export default function App () {
-  
-const [tasks, dispatch] = useReducer (tasksReducer, initialTasks)
-
-  function handleAddTask (text) {
-dispatch ({
-  type: 'added',
-  text: text,
-  id: nextId++
-})
+  function handleAddTask(text) {
+    dispatch({
+      type: "added",
+      text: text,
+      id: nextId++,
+    });
   }
 
-  function handleChangeTask (task) {
-    dispatch ({
-      type: 'changed',
+  function handleChangeTask(task) {
+    dispatch({
+      type: "changed",
       task: task,
-    })
-      }
+    });
+  }
 
-      function handleDeleteTask (taskId) {
-        dispatch ({
-          type: 'deleted',
-          id: taskId
-        })
-          }
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
+  }
 
   return (
     <div className="App">
-<AddTask onAddTask={handleAddTask}/>
-<TaskWrapper tasks={tasks}
+      <AddTask onAddTask={handleAddTask} />
+      <TaskWrapper
+        tasks={tasks}
         onChangeTask={handleChangeTask}
-        onDeleteTask={handleDeleteTask}/>
+        onDeleteTask={handleDeleteTask}
+      />
     </div>
   );
 }
 
 let initialTasks = [];
-let nextId = 0
+let nextId = 0;
